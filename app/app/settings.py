5f92f0a00139f8b2e939e7724c098ba8a6704c1a
danny nsengimana <59091829+dannynsengimana@users.noreply.github.com>
2024-02-16 12:31:31 +0200

    Created user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os 
+import os
 from pathlib import Path
 
 
@@ -129,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
